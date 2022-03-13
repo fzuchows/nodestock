b94a716972e8a2748bece81491c38d3a802e2a7a
frankz <fzuchows@gmail.com>
2022-03-13 19:33:09 +0100

    Initial version

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 5555;
+
+// Set handlebars middleware
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// Set handlebar routes
+//app.get('/', function(req, res) {
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// Set folder for static content 
+app.use(express.static(path.join(__dirname,'public')));
+
+// Start node: node index.js
+app.listen(PORT, () => console.log('Server listening on port: ' + PORT));
