23afb6b310e772dfb1e8465b82592608c021cb1e
frankz <fzuchows@gmail.com>
2022-03-21 00:35:13 +0100

    update node, js and create procfile

--- index.js
+++ index.js
@@ -1,22 +1,59 @@
 // Stock Market Portfolio App
 // Credit: John Elder, PacktPub, Build a Stock Market App with Node
+// Soure: https://github.com/flatplanet/nodestock
 const express = require('express');
+const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
-
-const app = express();
-
+const request = require('request');
+const bodyParser = require('body-parser');
+// get the port form the system env. OR hard-coded for testing
 const PORT = process.env.PORT || 5555;
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Call Stock API to retrieve Stock info with
+// API Key iexcloud.io: pk_5890a6d306c24f118f65c546bf20d7da
+// https://cloud.iexapis.com/stable/stock/aapl/quote?token=YOUR_TOKEN_HERE
+function callStockApi(finishedApi, stocksymb) {
+  request('https://cloud.iexapis.com/stable/stock/' + stocksymb + '/quote?token=pk_5890a6d306c24f118f65c546bf20d7da', {json:true}, (err,res, body) => {
+    if(err) {return console.log(err);}
+    if(res.statusCode===200) {
+      finishedApi(body);
+    }
+  });
+};
 
 // Set handlebars middleware
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-// Set handlebar routes
+// Set handlebar GET index routes
 //app.get('/', function(req, res) {
 app.get('/', (req, res) => {
-  res.render('home');
+  callStockApi(function(doneApi) {
+    res.render('home', {
+      stockinfo: doneApi
+    });
+  });
+});
+
+// Set handlebar POST index routes
+//app.get('/', function(req, res) {
+app.post('/', (req, res) => {
+  callStockApi(function(doneApi) {
+    //posted_stuff = req.body.stock_ticker;
+    res.render('home', {
+      stockinfo: doneApi
+    });
+  }, req.body.stock_ticker);
+});
+
+//app.get('/', function(req, res) {
+app.get('/about.html', (req, res) => {
+  res.render('about');
 });
 
 // Set folder for static content 
